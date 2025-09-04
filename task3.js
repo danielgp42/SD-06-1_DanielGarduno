ccbb5f5a081a271a17105088ea5359f0be1cfeb2
danielgp <danielgarduno282@gmail.com>
2025-09-04 16:40:37 -0600

    se resolvieron los primeros 4 tasks

--- task3.js
+++ task3.js
@@ -1,13 +1,16 @@
 // Type your code below this line!
 
-function Mail(subj, msg) {
+function Mail(subj, msg, printMail) {
     this.subject = subj
     this.message = msg
-    
+    this.printMail=function(){
+      console.log(this.subject+ ":"+this.message);
+      
+    }
   }
   
-  const newMail = new Mail(  )
-  
+  const newMail = new Mail(process.argv[2],process.argv[3] )
+  console.log(newMail.subject + ": " + newMail.message)
   // Type your code above this line!
   
   newMail.printMail()
--- task5.js
+++ task5.js
@@ -2,7 +2,14 @@
 
 // Type your code below this line!
 
-
-
+function FriendsList(amigos){
+this.amigos= function(){
+    const numberamigos= prompt("Ingrese el numero de amigos:")
+    for (let i=0; i<=amigos; i++){
+        const nombreAmigo= prompt("Ingresa el primer nombre: ");
+        this.amigos.push(nombreAmigo);
+    }
+}
+}
 // Type your code above this line!
 
